b531a672099098bf2b5005ea85dad3cf2a31413a
Joshua Kite <joshuamkite@gmail.com>
2018-09-10 15:21:33 +0100

    update other file

--- files/authorized_keys_command.sh
+++ files/authorized_keys_command.sh
@@ -34,10 +34,10 @@ then
 fi
 
 UnsaveUserName="$1"
-UnsaveUserName=${UnsaveUserName//"plus"/"+"}
-UnsaveUserName=${UnsaveUserName//"equal"/"="}
-UnsaveUserName=${UnsaveUserName//"comma"/","}
-UnsaveUserName=${UnsaveUserName//"at"/"@"}
+UnsaveUserName=${UnsaveUserName//".plus."/"+"}
+UnsaveUserName=${UnsaveUserName//".equal."/"="}
+UnsaveUserName=${UnsaveUserName//".comma."/","}
+UnsaveUserName=${UnsaveUserName//".at."/"@"}
 
 aws iam list-ssh-public-keys --user-name "$UnsaveUserName" --query "SSHPublicKeys[?Status == 'Active'].[SSHPublicKeyId]" --output text | while read -r KeyId; do
   aws iam get-ssh-public-key --user-name "$UnsaveUserName" --ssh-public-key-id "$KeyId" --encoding SSH --query "SSHPublicKey.SSHPublicKeyBody" --output text
